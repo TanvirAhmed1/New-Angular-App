78f7879bbb2297859a36e13562a981a3f887c42b
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-02-03 02:10:31 +0600

    components working

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,19 +6,48 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
-const MaterialComponents = [
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+const important = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
   MatToolbarModule,
   MatSidenavModule,
   MatMenuModule,
-  MatListModule
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatTabsModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  MatTooltipModule,
+  MatSnackBarModule,
 ];
 @NgModule({
 
-  imports: [MatButtonModule],
-  exports: [MatButtonModule]
+  imports: [important],
+  exports: [important]
 })
 export class MaterialModule { }
