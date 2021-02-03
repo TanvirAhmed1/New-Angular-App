6876a5e58f7311676aeccac3f0feba08869296a6
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-02-04 02:03:59 +0600

    Navbar and Recipe model Added

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a Test', 'https://www.travelanddestinations.com/wp-content/uploads/2019/11/Nasi-lemak-meal.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
